5d81f0ee01795d0b35812a17a35e04382bfbc600
soumi7 <soumibardhan10@gmail.com>
2020-04-07 17:22:38 +0530

    c1

--- codery.py
+++ codery.py
@@ -1,3 +1,4 @@
+'''
 class MyBotHandler(object):
     '''
     This plugin will allow users to get latest contents
@@ -50,3 +51,24 @@ class MyBotHandler(object):
 
 
 handler_class = MyBotHandler
+'''
+
+# See readme.md for instructions on running this code.
+
+from typing import Any, Dict
+
+class HelloWorldHandler(object):
+    def usage(self) -> str:
+        return '''
+        This is a boilerplate bot that responds to a user query with
+        "beep boop", which is robot for "Hello World".
+
+        This bot can be used as a template for other, more
+        sophisticated, bots.
+        '''
+
+    def handle_message(self, message: Dict[str, Any], bot_handler: Any) -> None:
+        content = 'beep boop'  # type: str
+        bot_handler.send_reply(message, content)
+
+handler_class = HelloWorldHandler
