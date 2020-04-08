98099fedabbc8b9e00be94ff441c6aab353d9dfc
soumi7 <soumibardhan10@gmail.com>
2020-04-08 16:11:31 +0530

    d1

--- codery.py
+++ codery.py
@@ -1,21 +1,43 @@
+# See readme.md for instructions on running this code.
+import logging
+from urllib import parse
 
 import requests
-import logging
-import sys
 
-from requests.exceptions import HTTPError, ConnectionError
-from typing import Dict, Any, Union, List, Tuple, Optional
+from bs4 import BeautifulSoup
 
+from typing import Dict, Any, Union, List
 
-commands_list = ('list', 'top', 'help')
+def google_search(keywords: str) -> List[Dict[str, str]]:
+    query = {'q': keywords}
+    # Gets the page
+    page = requests.get('http://www.google.com/search', params=query)
+    # Parses the page into BeautifulSoup
+    soup = BeautifulSoup(page.text, "lxml")
 
-class YoutubeHandler(object):
+    # Gets all search URLs
+    anchors = soup.find(id='search').findAll('a')
+    results = []
 
-    def usage(self) -> str:
-        return '''
-        
-        'Hey there! I am codery, your helper to competitive coding!'
-        '''
+    for a in anchors:
+        try:
+            # Tries to get the href property of the URL
+            link = a['href']
+        except KeyError:
+            continue
+        # Link must start with '/url?', as these are the search result links
+        if not link.startswith('/url?'):
+            continue
+        # Makes sure a hidden 'cached' result isn't displayed
+        if a.text.strip() == 'Cached' and 'webcache.googleusercontent.com' in a['href']:
+            continue
+        # a.text: The name of the page
+        result = {'url': "https://www.google.com{}".format(link),
+                  'name': a.text}
+        results.append(result)
+    return results
+
+def get_google_result(search_keywords: str) -> str:
     help_content = "*Help for Codery bot* :robot_face: : \n\n" \
                    "The bot responds to messages starting with @codery-bot.\n\n" \
                    "`@codery-bot <search contests>` will return top Contests for the given `<search term>`.\n" \
@@ -24,156 +46,54 @@ class YoutubeHandler(object):
                    "Example:\n" \
                    " * @codery-bot funny cats\n" \
                    " * @codery-bot list funny dogs"
+                   
+    help_message = "To use this bot, start messages with @mentioned-bot, \
+                    followed by what you want to search for. If \
+                    found, Zulip will return the first search result \
+                    on Google.\
+                    \
+                    An example message that could be sent is:\
+                    '@mentioned-bot zulip' or \
+                    '@mentioned-bot how to create a chatbot'."
 
-    def initialize(self, bot_handler: Any) -> None:
-        self.config_info = bot_handler.get_config_info('youtube')
-        # Check if API key is valid. If it is not valid, don't run the bot.
+    search_keywords = search_keywords.strip()
+
+    if search_keywords == 'help':
+        return help_message
+    elif search_keywords == '' or search_keywords is None:
+        return help_message
+    else:
         try:
-            search_youtube('test', self.config_info['key'], self.config_info['video_region'])
-        except HTTPError as e:
-            if (e.response.json()['error']['errors'][0]['reason'] == 'keyInvalid'):
-                bot_handler.quit('Invalid key.'
-                                 'Follow the instructions in doc.md for setting API key.')
-            else:
-                raise
-        except ConnectionError:
-            logging.warning('Bad connection')
+            results = google_search(search_keywords)
+            if (len(results) == 0):
+                return "Found no results."
+            return "Found Result: [{}]({})".format(results[0]['name'], results[0]['url'])
+        except Exception as e:
+            logging.exception(str(e))
+            return 'Error: Search failed. {}.'.format(e)
+
+class GoogleSearchHandler(object):
+    '''
+    This plugin allows users to enter a search
+    term in Zulip and get the top URL sent back
+    to the context (stream or private) in which
+    it was called. It looks for messages starting
+    with @mentioned-bot.
+    '''
+
+    def usage(self) -> str:
+        return '''
+            This plugin will allow users to search
+            for a given search term on Google from
+            Zulip. Use '@mentioned-bot help' to get
+            more information on the bot usage. Users
+            should preface messages with
+            @mentioned-bot.
+            '''
 
     def handle_message(self, message: Dict[str, str], bot_handler: Any) -> None:
+        original_content = message['content']
+        result = get_google_result(original_content)
+        bot_handler.send_reply(message, result)
 
-        if message['content'] == '' or message['content'] == 'help':
-            bot_handler.send_reply(message, self.help_content)
-        else:
-            cmd, query = get_command_query(message)
-            bot_response = get_bot_response(query,
-                                            cmd,
-                                            self.config_info)
-            logging.info(bot_response.format())
-            bot_handler.send_reply(message, bot_response)
-
-
-def search_youtube(query: str, key: str,
-                   region: str, max_results: int = 1) -> List[List[str]]:
-
-    videos = []
-    params = {
-        'part': 'id,snippet',
-        'maxResults': max_results,
-        'key': key,
-        'q': query,
-        'alt': 'json',
-        'type': 'video',
-        'regionCode': region}  # type: Dict[str, Union[str, int]]
-
-    url = 'https://www.googleapis.com/youtube/v3/search'
-    try:
-        r = requests.get(url, params=params)
-    except ConnectionError as e:  # Usually triggered by bad connection.
-        logging.exception('Bad connection')
-        raise
-    r.raise_for_status()
-    search_response = r.json()
-    # Add each result to the appropriate list, and then display the lists of
-    # matching videos, channels, and playlists.
-    for search_result in search_response.get('items', []):
-        if search_result['id']['kind'] == 'youtube#video':
-            videos.append([search_result['snippet']['title'],
-                           search_result['id']['videoId']])
-    return videos
-
-
-def get_command_query(message: Dict[str, str]) -> Tuple[Optional[str], str]:
-    blocks = message['content'].lower().split()
-    command = blocks[0]
-    if command in commands_list:
-        query = message['content'][len(command) + 1:].lstrip()
-        return command, query
-    else:
-        return None, message['content']
-
-
-def get_bot_response(query: Optional[str], command: Optional[str], config_info: Dict[str, str]) -> str:
-
-    key = config_info['key']
-    max_results = int(config_info['number_of_results'])
-    region = config_info['video_region']
-    video_list = []   # type: List[List[str]]
-    try:
-        if query == '' or query is None:
-            return YoutubeHandler.help_content
-        if command is None or command == 'top':
-            video_list = search_youtube(query, key, region)
-
-        elif command == 'list':
-            video_list = search_youtube(query, key, region, max_results)
-
-        elif command == 'help':
-            return YoutubeHandler.help_content
-
-    except (ConnectionError, HTTPError):
-        return 'Uh-Oh, couldn\'t process the request ' \
-               'right now.\nPlease again later'
-
-    reply = 'Here is what I found for `' + query + '` : '
-
-    if len(video_list) == 0:
-        return 'Oops ! Sorry I couldn\'t find any video for  `' + query + '` :slightly_frowning_face:'
-    elif len(video_list) == 1:
-        return (reply + '\n%s - [Watch now](https://www.youtube.com/watch?v=%s)' % (video_list[0][0], video_list[0][1])).strip()
-
-    for title, id in video_list:
-        reply = reply + \
-            '\n * %s - [Watch now](https://www.youtube.com/watch/%s)' % (title, id)
-    # Using link https://www.youtube.com/watch/<id> to
-    # prevent showing multiple previews
-    return reply
-
-
-handler_class = YoutubeHandler
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-
-class HelloWorldHandler(object):
-    def usage(self) -> str:
-        return 
-        This is a boilerplate bot that responds to a user query with
-        "beep boop", which is robot for "Hello World".
-
-        This bot can be used as a template for other, more
-        sophisticated, bots.
-        
-
-    def handle_message(self, message: Dict[str, Any], bot_handler: Any) -> None:
-        content = 'Hey there! I am codery!'  # type: str
-        bot_handler.send_reply(message, content)
-
-handler_class = HelloWorldHandler
-
-'''
+handler_class = GoogleSearchHandler
