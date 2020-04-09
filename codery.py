1232f91a1ecff35c63de442e40ee78038fcc2529
soumi7 <soumibardhan10@gmail.com>
2020-04-09 12:19:07 +0530

    commit changes

--- codery.py
+++ codery.py
@@ -11,9 +11,11 @@ from typing import Dict, Any, Union, List
 def google_search(keywords: str) -> List[Dict[str, str]]:
     query = {'q': keywords}
     # Gets the page
-    page = requests.get('http://www.google.com/search', params=query)
+    page = requests.get('https://www.hackerearth.com/challenges', params=query)
     # Parses the page into BeautifulSoup
-    soup = BeautifulSoup(page.text, "lxml")
+    #soup = BeautifulSoup(page.text, "lxml")
+
+    
 
     # Gets all search URLs
     anchors = soup.find(id='search').findAll('a')
@@ -38,19 +40,37 @@ def google_search(keywords: str) -> List[Dict[str, str]]:
     return results
 
 def get_google_result(search_keywords: str) -> str:
-    help_message = "To use this bot, start messages with @mentioned-bot, \
-                    followed by what you want to search for. If \
-                    found, Zulip will return the first search result \
-                    on Google.\
-                    \
-                    An example message that could be sent is:\
-                    '@mentioned-bot zulip' or \
-                    '@mentioned-bot how to create a chatbot'."
+    help_message = "*Help for Codery bot* :robot_face: : \n\n" \
+                   "The bot responds to messages starting with @codery-bot.\n\n" \
+                   "`@codery-bot <search contests>` will return top Contests for the given `<search term>`.\n" \
+                   "`@codery-bot top <search terms>` also returns the top Contest result.\n" \
+                   "`@codery-bot list <search terms>` will return a list of contests for the given <search term>.\n \n" \
+                   "Example:\n" \
+                   " * @codery-bot funny cats\n" \
+                   " * @codery-bot list funny dogs"
+
+    
 
     search_keywords = search_keywords.strip()
 
     if search_keywords == 'help':
         return help_message
+    elif search_keywords =='contests':
+        
+
+        URL = 'https://www.stopstalk.com/contests'
+
+
+        page = requests.get(URL)
+        content = requests.get(URL)
+        soup = BeautifulSoup(content.text, 'html.parser')
+        contentTable  = soup.find('table', { "class" : "centered bordered"}) # Use dictionary to pass key : value pair
+        rows  = contentTable.find_all('tr')
+        l=""
+        for row in rows:
+            l+=row.get_text()
+        return l
+
     elif search_keywords == '' or search_keywords is None:
         return help_message
     else:
@@ -69,7 +89,7 @@ class GoogleSearchHandler(object):
     term in Zulip and get the top URL sent back
     to the context (stream or private) in which
     it was called. It looks for messages starting
-    with @mentioned-bot.
+    with @codery-bot.
     '''
 
     def usage(self) -> str:
@@ -79,7 +99,7 @@ class GoogleSearchHandler(object):
             Zulip. Use '@mentioned-bot help' to get
             more information on the bot usage. Users
             should preface messages with
-            @mentioned-bot.
+            @codery-bot.
             '''
 
     def handle_message(self, message: Dict[str, str], bot_handler: Any) -> None:
